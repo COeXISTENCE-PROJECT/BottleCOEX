6558a46a8424963bef48e1dd9104e88120051c6c
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2024-07-29 10:42:27 +0200

    params - keychain

---
+++ keychain.py
@@ -0,0 +1,65 @@
+class keychain:
+
+    ########### File names ###########
+    INPUT_FILE = "params.json"
+
+
+    ############## EDATA ##############
+    NAME = "name"
+    Xaxis = "Xaxis"
+    Yaxis = "Yaxis"
+    XaxisName = "XaxisName"
+    YaxisName = "YaxisName"
+    Xval = "Xval"
+    Yval = "Yval"
+
+
+    ############ Net2Plain ############
+    LINKS = "Links"
+    ROUTES = "Routes"
+    ROUTE_LABELS = "RouteLabels"
+
+
+    ############ Net3twoEqual ##########
+    LINKS = "Links"
+    ROUTES = "Routes"
+    ROUTE_LABELS = "RouteLabels"
+
+
+    ############## NetBraess / NetBraess2 ###########
+    LINKS = "Links"
+    NUMBER_OF_LINKS = "NumberOfLinks"
+    CAPACITIES = "Capacities"
+    POWERS = "Powers"
+    T0LINKS = "t0links"
+    KAPPAS = "kappas" 
+    ROUTES = "Routes"
+    NUMBER_OF_ROUTES = "NumberOfRoutes"
+    LINKS_MATRIX = "LinksMatrix"
+    ROUTE_LABELS = "RouteLabels"
+    
+    ############## ParamsData ############
+    TOTAL_VEHICLES = "totalVehicles"
+    HDVS = "HDVs"
+    DEFAULT_ALPHA_ZERO = "defaultAlphaZero"
+    DEFAULT_EPSILON = "defaultEpsilon"
+    MULTIPLIER = "multiplier"
+    DAY_RANGE = "dayRange"
+    ROAD_NETWORKS = "RoadNetworks"
+    NETWORK_DAY_CHANGE = "NetworkDayChange"
+    DEFAULT_MODEL_NAME = "defaultModelName"
+    DEFAULT_LOGIT_PARAM = "defaultLogitParam"
+    DEFAULT_INITIAL_KNOWLEDGE = "defaultInitialKnowledge"
+    DEFAULT_INITIAL_CHOICE = "defaultInitialChoice"
+    DEFAULT_ONLY_EXPERIENCE = "defaultOnlyExperience"
+    DEFAULT_FLEET_SIZE = "defaultFleetSize"
+    DEFAULT_FLEET_INTRODUCTION = "defaultFleetIntroduction"
+    DEFAULT_FLEET_OPTIMIZATION_ONSET = "defaultFleetOptimizationOnset"
+    DEFAULT_FLEET_OPTIMIZATION_END = "defaultFleetOptimizationEnd"
+    DEFAULT_LAMBDA_CAV = "defaultLambdaCAV"
+    DEFAULT_LAMBDA_HDV = "defaultLambdaHDV"
+    DEFAULT_CAV_TARGET = "defaultCAVTarget"
+    DEFAULT_FLEET_MODE = "defaultFleetMode"
+    MOVING_AVERAGE_STEP = "MovingAverageStep"
+    PLOT_CAVS = "PlotCAVs"
+    PLOT_MEAN_HDV = "PlotMeanHDV"
