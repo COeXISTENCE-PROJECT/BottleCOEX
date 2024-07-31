b74f08387d34d7dedf9b5ab8fb5018a35b1c655d
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2024-07-31 16:22:12 +0200

    simulator improv

--- utilities/simulator.py
+++ utilities/simulator.py
@@ -30,36 +30,78 @@ def Simulator(RoadNetworks, NetworkDayChange, NL, Links, NR, Routes, human_param
               LambdaCAV = float(data_params[kc.DEFAULT_LAMBDA_CAV]), CAVTarget = (data_params[kc.DEFAULT_CAV_TARGET]), 
               jrange = data_params[kc.DAY_RANGE], totalND = int(float(data_params[kc.TOTAL_VEHICLES]))):
     
+    ## Initialize parameters
     mult = params_data[kc.MULTIPLIER]
+    totalND = int(mult * totalND)
+    Fleet_size = int(totalND * Fleet_size)
 
-    #####Normalize variables by mult
-    totalND = int(mult*(totalND))
-    Fleet_size = int((totalND) * Fleet_size)
-    #####Create human drivers########
+    ## Initialize human drivers list
     HDV = []
     Qinit = np.zeros(NR)
+
+    # Create human drivers
     for i in range(totalND):
         INITIAL = Initial_Knowledge
         INITIALCHOICE = Initial_Choice
-        for r in range(NR):  
-            if(INITIAL == 'FreeFlow'):      Qinit[r] = Routes[r].t0
-            elif(INITIAL == 'Pessimistic'): Qinit[r] = 25.0
-            elif(INITIAL == 'Optimistic'):  Qinit[r] = 0.0
 
-        HDV.append(HumanAgent(human_params, jrange, Model_Name, Alpha_Zero, human_params[kc.LEARNING_RATE_DECAY], Epsilon_Zero,
-                              human_params[kc.EXPLORATION_RATE_DECAY], Qinit, INITIALCHOICE, Logit_Exp_Coeff, Only_Experience, NR))
+        for r in range(NR):
+            if INITIAL == 'FreeFlow':
+                Qinit[r] = Routes[r].t0
+            elif INITIAL == 'Pessimistic':
+                Qinit[r] = 25.0
+            elif INITIAL == 'Optimistic':
+                Qinit[r] = 0.0
+
+        HDV.append(
+            HumanAgent(
+                human_params, 
+                jrange, 
+                Model_Name, 
+                Alpha_Zero, 
+                human_params[kc.LEARNING_RATE_DECAY], 
+                Epsilon_Zero,
+                human_params[kc.EXPLORATION_RATE_DECAY], 
+                Qinit, 
+                INITIALCHOICE, 
+                Logit_Exp_Coeff, 
+                Only_Experience, 
+                NR
+            )
+        )
+
         
-    ####Create fleet agent
-    if(CAVTarget == 'Selfish'): LambdaHDV, LambdaCAV = 0.0, 1.0 
-    elif(CAVTarget == 'Malicious'): LambdaHDV, LambdaCAV = -1.0, 0.0 
-    elif(CAVTarget == 'Disruptive'): LambdaHDV, LambdaCAV = -9.0, 1.0 
-    elif(CAVTarget == 'Social'): LambdaHDV, LambdaCAV = 1.0, 1.0 
-    elif(CAVTarget == 'Altruistic'): LambdaHDV, LambdaCAV = 1.0, 0.0 
+    ## Create fleet agent
+    if CAVTarget == 'Selfish':
+        LambdaHDV, LambdaCAV = 0.0, 1.0
+    elif CAVTarget == 'Malicious':
+        LambdaHDV, LambdaCAV = -1.0, 0.0
+    elif CAVTarget == 'Disruptive':
+        LambdaHDV, LambdaCAV = -9.0, 1.0
+    elif CAVTarget == 'Social':
+        LambdaHDV, LambdaCAV = 1.0, 1.0
+    elif CAVTarget == 'Altruistic':
+        LambdaHDV, LambdaCAV = 1.0, 0.0
+    else:
+        # Handle unexpected CAVTarget values
+        raise ValueError(f"Unexpected CAVTarget value: {CAVTarget}")
 
-    F1 = FleetOperatorAgent(NR, Routes, NL, Links, Fleet_size, Fleet_introduction, LambdaHDV, LambdaCAV, Fleet_Mode)        
-    ####Initialize other variables
+
+    ## Initialize the FleetOperatorAgent
+    F1 = FleetOperatorAgent(
+        NR, 
+        Routes, 
+        NL, 
+        Links, 
+        Fleet_size, 
+        Fleet_introduction, 
+        LambdaHDV, 
+        LambdaCAV, 
+        Fleet_Mode
+    )
+
+    ## Initialize other variables
     averagingSampleSize = totalND - F1.FS
-    routeTimes = np.zeros((jrange,NR))                                
+    routeTimes = np.zeros((jrange, NR))                                
     averageHDVTravelTime = np.zeros(jrange)                            
     averageCAVTravelTime = np.zeros(jrange)                           
     averagePerceivedTravelTime = np.zeros(jrange)                      
@@ -69,121 +111,179 @@ def Simulator(RoadNetworks, NetworkDayChange, NL, Links, NR, Routes, human_param
     noChangeRoutes = np.zeros(jrange)               
     qcountsHDV = np.zeros((jrange, NR), dtype=int)                      
     qcountsCAV = np.zeros((jrange, NR), dtype=int)                      
+
+    ## Update qcountsHDV for initial routes
     for i in range(totalND):
         qcountsHDV[0, HDV[i].curRoute] += 1
 
-    ND = np.zeros(jrange, dtype = int)
-    ND[0] = totalND  #we assume that there is no fleet on day 0
+    ## Initialize network demand and other variables
+    ND = np.zeros(jrange, dtype=int)
+    ND[0] = totalND 
     curNetwork = 0
-    noDriversChangingRoutes = 0                                                                       
+    noDriversChangingRoutes = 0
 
-    #########Main loop over days############
+
+
     random.seed()
+    # Main loop over days
     for j in range(jrange):
-
-        #Evaluate route times on day j
-        linksCounts = routesCounts2linksCounts(NR, Routes, NL, Links, (qcountsHDV[j, :] + qcountsCAV[j, :]))
+        # Evaluate route times for day j
+        linksCounts = routesCounts2linksCounts(
+            NR, Routes, NL, Links, (qcountsHDV[j, :] + qcountsCAV[j, :])
+        )
         for r in range(NR):
+            # Calculate travel time and add random noise
             routeTimes[j, r] = Routes[r].calculate_travel_time(Links, linksCounts)
-            routeTimes[j, r] += np.random.normal(loc=0.0, scale=human_params[kc.ROUTE_RANDOM_VAR])
+            routeTimes[j, r] += np.random.normal(
+                loc=0.0, scale=human_params[kc.ROUTE_RANDOM_VAR]
+            )
 
-        #Save statistics for day j
-        noChangeRoutes[j] = noDriversChangingRoutes                                                  
-        if(ND[j]>0):
-            averageHDVTravelTime[j] = np.sum(routeTimes[j, :] * qcountsHDV[j, :])/ND[j] 
+        # Save statistics for day j
+        noChangeRoutes[j] = noDriversChangingRoutes
+        if ND[j] > 0:
+            averageHDVTravelTime[j] = np.sum(routeTimes[j, :] * qcountsHDV[j, :]) / ND[j]
 
-        if(F1.during_optimization(j)):
+        if F1.during_optimization(j):
             CAVTotal = F1.FS
-            if(CAVTotal > 0):
-                averageCAVTravelTime[j] = np.sum(routeTimes[j, :]*qcountsCAV[j,:]) / CAVTotal
-            storedTarget[j] = (F1.Target((ND[j]*averageHDVTravelTime[j]), (CAVTotal*averageCAVTravelTime[j])))/(totalND)
-            averageTravelTime[j] = (np.sum(routeTimes[j, :]*qcountsHDV[j, :]) + np.sum(routeTimes[j, :]*qcountsCAV[j,:]))/totalND
+            if CAVTotal > 0:
+                averageCAVTravelTime[j] = np.sum(routeTimes[j, :] * qcountsCAV[j, :]) / CAVTotal
+            storedTarget[j] = F1.Target(
+                (ND[j] * averageHDVTravelTime[j]),
+                (CAVTotal * averageCAVTravelTime[j])
+            ) / totalND
+            averageTravelTime[j] = (
+                np.sum(routeTimes[j, :] * qcountsHDV[j, :]) +
+                np.sum(routeTimes[j, :] * qcountsCAV[j, :])
+            ) / totalND
         else:
             averageTravelTime[j] = averageHDVTravelTime[j]
 
-        if((Model_Name == 'Gumbel') or (Model_Name == 'GumbelEps')):
-            averagePerceivedTravelTime[j] = 0                                                            
-            #Perceived travel time only for HDVs which do not mutate into CAVs
+        # Handle perceived travel time
+        if Model_Name in ['Gumbel', 'GumbelEps']:
+            averagePerceivedTravelTime[j] = 0
             for i in range(averagingSampleSize):
-                averagePerceivedTravelTime[j] += (routeTimes[j, HDV[i].curRoute] + HDV[i].gumbelOffsets[HDV[i].curRoute])  
-            if (averagingSampleSize > 0):
-                averagePerceivedTravelTime[j] = averagePerceivedTravelTime[j] / (averagingSampleSize)         
+                averagePerceivedTravelTime[j] += (
+                    routeTimes[j, HDV[i].curRoute] + HDV[i].gumbelOffsets[HDV[i].curRoute]
+                )
+            if averagingSampleSize > 0:
+                averagePerceivedTravelTime[j] /= averagingSampleSize
         else:
             averagePerceivedTravelTime[j] = -1
 
-        #Don't learn or make future choices if it's the last day
-        if((j==(jrange-1))): break    
+        # Skip learning and route choice if it's the last day
+        if j == (jrange - 1):
+            break
 
-        #Update number of human drivers ND (the last F1.FS become fleet vehicles)
-        if(F1.during_optimization(j+1)): ND[j+1] = totalND - F1.FS                                                                
-        else: ND[j+1] = totalND
+        # Update number of human drivers (last F1.FS become fleet vehicles)
+        if F1.during_optimization(j + 1):
+            ND[j + 1] = totalND - F1.FS
+        else:
+            ND[j + 1] = totalND
 
-        #Update network
+        # Update network if necessary
         try:
-            if(j == NetworkDayChange[curNetwork]):
+            if j == NetworkDayChange[curNetwork]:
                 curNetwork += 1
-                fN = open(datadirpath + RoadNetworks[curNetwork] + ".json")
-                net_params = json.load(fN)
-                fN.close()
+                with open(datadirpath + RoadNetworks[curNetwork] + ".json") as fN:
+                    net_params = json.load(fN)
                 NL, Links = createNetwork(net_params, mult)
-        except:
-            raise(Exception("Network update failed"))
+        except Exception as e:
+            raise Exception(f"Network update failed: {e}")
 
-        #######Main iteration over drivers - learning and route choice###################################
-        noDriversChangingRoutes = 0                                                                       
+        # Main iteration over drivers - learning and route choice
+        noDriversChangingRoutes = 0
         OLDHDV = copy.deepcopy(HDV)
-        for i in range(ND[j+1]):                                                                 
-            HDV[i].learn(routeTimes[j,:])
-            HDV[i].update_rates()                                                          
-            oldRoute = HDV[i].curRoute                                                      
+        for i in range(ND[j + 1]):
+            HDV[i].learn(routeTimes[j, :])
+            HDV[i].update_rates()
+            oldRoute = HDV[i].curRoute
             HDV[i].make_next_route_choice()
-            qcountsHDV[j+1, HDV[i].curRoute] += 1
-            if(oldRoute != HDV[i].curRoute): noDriversChangingRoutes += 1
-        ######end of iteration over drivers, optimization by Fleet
+            qcountsHDV[j + 1, HDV[i].curRoute] += 1
+            if oldRoute != HDV[i].curRoute:
+                noDriversChangingRoutes += 1
+
+        # Optimization by Fleet
         NEWHDV = copy.deepcopy(HDV)
-        F1.make_next_routes_choice(j+1, Routes, Links, NEWHDV, ND[j+1], qcountsHDV[j+1], qcountsHDV[j], routeTimes[j,:], OLDHDV) 
-        qcountsCAV[j+1, :] = F1.Intercepts[:]
+        F1.make_next_routes_choice(
+            j + 1, Routes, Links, NEWHDV, ND[j + 1], qcountsHDV[j + 1], qcountsHDV[j], routeTimes[j, :], OLDHDV
+        )
+        qcountsCAV[j + 1, :] = F1.Intercepts[:]
     ######End of main loop over days
 
     ####Process data for output
     convVector = np.ones(params_data["MovingAverageStep"]) / params_data["MovingAverageStep"]
 
 
+    # Calculate cumulative quantities for HDVs and travel times
     for r in range(NR):
-        qcI = qcountsHDV[:,r]
-        convol = np.convolve(qcI, convVector)
+        qcI = qcountsHDV[:, r]
+        convol = np.convolve(qcI, convVector, mode='full')
         for j in range(jrange):
-            CumqcountsHDV[j,r] = convol[j]
-    CumaverageCAVTravelTime = np.convolve(averageCAVTravelTime, convVector)
-    CumaverageTravelTime = np.convolve(averageHDVTravelTime, convVector)
-    CumaveragePerceivedTravelTime = np.convolve(averagePerceivedTravelTime, convVector)
-    MovAvstoredTarget = np.convolve(storedTarget, convVector)
+            CumqcountsHDV[j, r] = convol[j]
+
+    # Calculate cumulative moving averages
+    CumaverageCAVTravelTime = np.convolve(averageCAVTravelTime, convVector, mode='full')
+    CumaverageTravelTime = np.convolve(averageHDVTravelTime, convVector, mode='full')
+    CumaveragePerceivedTravelTime = np.convolve(averagePerceivedTravelTime, convVector, mode='full')
+    MovAvstoredTarget = np.convolve(storedTarget, convVector, mode='full')
+
+    # Initialize cumulative stored target array
     CumstoredTarget = np.zeros(jrange)
     OptMaxIndex = jrange
     OptimizationOnset = Fleet_introduction
-    CumstoredTarget[OptimizationOnset:OptMaxIndex] = storedTarget[OptimizationOnset:OptMaxIndex] / (OptMaxIndex-OptimizationOnset)
-    for j in range(OptimizationOnset, OptMaxIndex): CumstoredTarget[j] += CumstoredTarget[j-1]
 
+    # Calculate cumulative stored target values
+    CumstoredTarget[OptimizationOnset:OptMaxIndex] = storedTarget[OptimizationOnset:OptMaxIndex] / (OptMaxIndex - OptimizationOnset)
+    for j in range(OptimizationOnset + 1, OptMaxIndex):
+        CumstoredTarget[j] += CumstoredTarget[j - 1]
 
-    TTdf = pd.DataFrame(data = np.concatenate((np.arange(jrange)[:,None],routeTimes,storedTarget[:,None],
-                                               CumstoredTarget[:,None],averageHDVTravelTime[:,None],
-                                               averagePerceivedTravelTime[:,None],averageCAVTravelTime[:,None],
-                                               averageTravelTime[:,None],
-                                               CumaverageTravelTime[:jrange,None],
-                                               CumaverageCAVTravelTime[:jrange,None],
-                                               CumaveragePerceivedTravelTime[:jrange,None],),axis=1),
-                        columns = ["Day",] + ["Travel time on " + Routes[r].label for r in range(NR)]
-                                           + ["Target", "CumulativeTarget", "Mean HDV travel time",
-                                              "Mean HDV Perceived travel time", "Mean CAV travel time",
-                                              "Mean travel time of all vehicles", "Moving Average Mean HDV travel time",
-                                              "Moving Average Mean CAV travel time", "Moving Average Mean HDV Perceived travel time"])
-    VCdf = pd.DataFrame(data = np.concatenate((np.arange(jrange)[:,None],qcountsHDV, CumqcountsHDV, qcountsCAV,noChangeRoutes[:,None]
-                                               ), axis=1),
-                        columns = ["Day",] + ["HDV count on " + Routes[r].label for r in range(NR)]
-                                           + ["MovAveHDV count on " + Routes[r].label for r in range(NR)]
-                                           + ["CAV count on " + Routes[r].label for r in range(NR)]
-                                           + ["Number of HDVs changing Routes",]
-                                            )
+    # Create DataFrame for travel times and related metrics
+    TTdf = pd.DataFrame(
+        data=np.concatenate((
+            np.arange(jrange)[:, None],  # Day
+            routeTimes,  # Travel times for each route
+            storedTarget[:, None],  # Target values
+            CumstoredTarget[:, None],  # Cumulative target values
+            averageHDVTravelTime[:, None],  # Average HDV travel time
+            averagePerceivedTravelTime[:, None],  # Average perceived travel time for HDVs
+            averageCAVTravelTime[:, None],  # Average CAV travel time
+            averageTravelTime[:, None],  # Average travel time for all vehicles
+            CumaverageTravelTime[:jrange, None],  # Moving average of HDV travel time
+            CumaverageCAVTravelTime[:jrange, None],  # Moving average of CAV travel time
+            CumaveragePerceivedTravelTime[:jrange, None]  # Moving average of perceived travel time
+        ), axis=1),
+        columns=[
+            "Day"
+        ] + [f"Travel time on {Routes[r].label}" for r in range(NR)] +
+        [
+            "Target",
+            "CumulativeTarget",
+            "Mean HDV travel time",
+            "Mean HDV Perceived travel time",
+            "Mean CAV travel time",
+            "Mean travel time of all vehicles",
+            "Moving Average Mean HDV travel time",
+            "Moving Average Mean CAV travel time",
+            "Moving Average Mean HDV Perceived travel time"
+        ]
+    )
+
+    # Create DataFrame for vehicle counts and route changes
+    VCdf = pd.DataFrame(
+        data=np.concatenate((
+            np.arange(jrange)[:, None],  # Day
+            qcountsHDV,  # HDV counts for each route
+            CumqcountsHDV,  # Cumulative HDV counts for each route
+            qcountsCAV,  # CAV counts for each route
+            noChangeRoutes[:, None]  # Number of HDVs changing routes
+        ), axis=1),
+        columns=[
+            "Day"
+        ] + [f"HDV count on {Routes[r].label}" for r in range(NR)] +
+        [f"MovAveHDV count on {Routes[r].label}" for r in range(NR)] +
+        [f"CAV count on {Routes[r].label}" for r in range(NR)] +
+        ["Number of HDVs changing Routes"]
+    )
         
     return ((TTdf, VCdf))
 
